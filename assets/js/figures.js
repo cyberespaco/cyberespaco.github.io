ed255bed5b1e8917811a89ca5b9cd21a1905250e
Pedro <pedromxavier@poli.ufrj.br>
2020-07-18 01:01:03 -0300

    Fix js

--- assets/js/figures.js
+++ assets/js/figures.js
@@ -10,7 +10,7 @@ function shuffle(array) {
 }
 
 function get_figures (n) { // -> Array
-    let figures = [];
+    var figures = [];
     for(let i=0; i<n; i++){
         let num = i.toString().padStart(2, "0");
         figures.push(`../images/vaporwave/img${num}.png`);
