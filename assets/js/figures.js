3de35842918062930634b08b7086b460e6558e9a
Pedro <pedromxavier@poli.ufrj.br>
2020-07-20 16:05:04 -0300

    Update page content

--- assets/js/figures.js
+++ assets/js/figures.js
@@ -9,7 +9,7 @@ function shuffle(array) {
     return array;
 }
 
-function get_figures (n) { // -> Array
+function get_photos (n) { // -> Array
     var figures = [];
     for(let i=0; i<n; i++){
         let num = i.toString().padStart(2, "0");
@@ -20,14 +20,23 @@ function get_figures (n) { // -> Array
 }
 
 function set_figures(n) {
-    var elements = document.getElementsByClassName("photo-image");
-    var figures = get_figures(n);
+    var photos = document.getElementsByClassName("photo-image");
+    var figures = get_photos(n);
     // Shuffles them for some randomness
     figures = shuffle(figures);
     
-    for(var i=0; i<elements.length; i++) {
-        if (elements[i].src === "") {
-            elements[i].src = figures[i % figures.length];
+    for(var i=0; i<photos.length; i++) {
+        if (photos[i].src === "") {
+            photos[i].src = figures[i % figures.length];
+        }
+    }
+
+    var courses = document.getElementsByClassName("course-image");
+    var course_figure = 'https://cyberespaco.github.io/assets/images/vaporwave/embreve.png';
+
+    for(var i=0; i<courses.length; i++) {
+        if (courses[i].src === "") {
+            courses[i].src = course_figure;
         }
     }
 }
