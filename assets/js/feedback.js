5bc9eaaea999c1ca282d455cda7f1cd27ba85a67
Pedro <pedromxavier@poli.ufrj.br>
2020-07-27 18:56:43 -0300

    Add Js extra support

--- assets/js/feedback.js
+++ assets/js/feedback.js
@@ -1,31 +1,34 @@
-function buildFeedback(array) {
+/* This requires jQuery to work
+**
+*/
+function buildFeedback(filePath) {
+
     let element = document.getElementById("feedback");
 
-    let innerHTML = new String();
+    var innerHTML = new String();
 
-    for (let i=0; i<array.lenght; i++) {
-        let json = array[i];
-
-        innerHTML += html`
-            <div class="feature">
-                <div class="image rounded"><img class="photo-image"/></div>
-                <div class="content">
-                    <header>
-                        <h4>${json[name]}</h4>
-                        <p>${json[caption]}</p>
-                    </header>
-                    <p>${json[comment]}</p>
+    $.getJSON(filePath, function( array ) {
+        for (let i=0; i<array.lenght; i++) {
+            let json = array[i];
+    
+            innerHTML += html`
+                <div class="feature">
+                    <div class="image rounded"><img class="photo-image"/></div>
+                    <div class="content">
+                        <header>
+                            <h4>${json[name]}</h4>
+                            <p>${json[caption]}</p>
+                        </header>
+                        <p>${json[comment]}</p>
+                    </div>
                 </div>
-            </div>
-            `
-    }
-
+                `
+        }
+    });
+    
     element.innerHTML = html`
     <div class="feature-grid">
 		${innerHTML}
     </div>
     `
 }
-
-
-
