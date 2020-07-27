25c32450bc32e81dd53ba61d4bd6af9749a8180b
Pedro <pedromxavier@poli.ufrj.br>
2020-07-27 01:58:12 -0300

    <3

--- assets/js/carddeck.js
+++ assets/js/carddeck.js
@@ -1,5 +1,11 @@
 function scrollToMe(element){
     let parent = element.parentNode;
     
-    parent.scrollLeft = element.offsetLeft - (element.clientWidth / 2);
+    parent.scrollLeft = element.offsetLeft - (0.65 * element.clientWidth );
+}
+
+var cards = document.getElementsByClassName("card");
+
+for (let i=0; i<cards.length; i++) {
+    cards[i].onclick = function() {scrollToMe(cards[i])};
 }
