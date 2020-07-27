61d17e7a2fe52547fa98688e34de6a5d98515095
Pedro <pedromxavier@poli.ufrj.br>
2020-07-27 02:36:43 -0300

    Set default scroll

--- assets/js/carddeck.js
+++ assets/js/carddeck.js
@@ -4,8 +4,14 @@ function scrollToMe(element){
     parent.scrollLeft = element.offsetLeft - (0.65 * element.clientWidth );
 }
 
-var cards = document.getElementsByClassName("card");
+let cards = document.getElementsByClassName("card");
 
 for (let i=0; i<cards.length; i++) {
     cards[i].onclick = function() {scrollToMe(cards[i])};
+}
+
+let actual_cards = document.getElementsByClassName("card actual");
+
+for (let i=0; i<actual_cards.length; i++) {
+    scrollToMe(actual_cards[i]);
 }
