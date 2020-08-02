abd54f8b9acf67b3043b2cf3a866f4fbad7b3d68
Pedro <pedromxavier@poli.ufrj.br>
2020-08-02 19:19:14 -0300

    :S

--- assets/js/carddeck.js
+++ assets/js/carddeck.js
@@ -7,14 +7,18 @@ function scrollToMe(element){
     });
 }
 
-let cards = document.getElementsByClassName("card");
+function makeScrollable(){
+    let cards = document.getElementsByClassName("card");
 
-for (let i=0; i<cards.length; i++) {
-    cards[i].onclick = function() {scrollToMe(cards[i])};
+    for (let i=0; i<cards.length; i++) {
+        cards[i].onclick = function() {scrollToMe(cards[i])};
+    }
 }
 
-let actual_cards = document.getElementsByClassName("card actual");
+function scrollToActual(){
+    let actual_cards = document.getElementsByClassName("card actual");
 
-for (let i=0; i<actual_cards.length; i++) {
-    scrollToMe(actual_cards[i]);
+    for (let i=0; i<actual_cards.length; i++) {
+        scrollToMe(actual_cards[i]);
+    }
 }
