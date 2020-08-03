ff3b1268cd7b5a38b5cf3bf651d92b3c2227edb1
Pedro <pedromxavier@poli.ufrj.br>
2020-08-03 20:00:02 -0300

    Update archive.js

---
+++ assets/js/archive.js
@@ -0,0 +1,58 @@
+var archivePlaceholder = {
+    "title" : "",
+    "img" : null,
+    "author" : "",
+    "text" : "",
+    "youtube" : "",
+    "github" : "",
+    "mail" : "",
+    "link" : ""
+}
+
+function archiveGet(json, key) {
+    if (key === undefined || key === null || !(key in json)) {
+        return archivePlaceholder[key];
+    }
+    else {
+        return json[key];
+    }
+}
+
+function buildArchive(filePath) {
+    $.getJSON( filePath, function ( array ) {
+
+        let element = document.getElementById("archive");
+        var innerHTML = new String();
+
+        for (let i=0; i<array.length; i++) {
+            let json = array[i];
+            
+            innerHTML = innerHTML + `
+            <div class="feature">
+                <div class="image rounded">
+                    <img class="course-image" src="${archiveGet(json, "img")}" alt="" />
+                </div>
+                <div class="content">
+                    <header>
+                        <h4>${archiveGet(json, "title")}</h4>
+                        <p>${archiveGet(json, "author")}</p>
+                    </header>
+                    <p>${archiveGet(json, "text")}</p>
+                    <ul class="icons">
+                        <li><a href="${archiveGet(json, "github")}" class="icon fa-github"><span class="label">Github</span></a></li>
+                        <li><a href="${archiveGet(json, "youtube")}" target="_blank" class="icon fa-youtube-play"><span class="label">YouTube</span></a></li>
+                        <li><a href="${archiveGet(json, "link")}" class="icon fa-link"><span class="label">Link</span></a></li>
+                        <li><a href="${archiveGet(json, "mail")}" class="icon fa-envelope-o"><span class="label">Mail</span></a></li>
+                    </ul>
+                </div>
+            </div>
+                `;
+        }
+
+        element.innerHTML = `
+            <div class="feature-grid">
+                ${innerHTML}
+            </div>
+            `;
+    });
+}
--- assets/js/carddeck.js
+++ assets/js/carddeck.js
@@ -22,3 +22,41 @@ function scrollToActual(){
         scrollToMe(actual_cards[i]);
     }
 }
+
+function buildCardDeck(filePath){
+    $.getJSON(filePath, function( array ) {
+        
+        let element = document.getElementById("feedback");
+        var innerHTML = new String();
+
+        for (let i=0; i<array.length; i++) {
+            let json = array[i];
+            
+            let src = (("img" in json) && (json["img"] !== undefined) && (json["img"] !== null))
+                        ? `src="${json['img']}"`
+                        : "";
+
+            innerHTML = innerHTML + `
+                <div class="feature">
+                    <div class="image rounded"><img ${src} class="photo-image"/></div>
+                    <div class="content">
+                        <header>
+                            <h4>${json["name"]}</h4>
+                            <p>${json["caption"]}</p>
+                        </header>
+                        <p>${json["comment"]}</p>
+                    </div>
+                </div>
+                `;
+        }
+
+        element.innerHTML = `
+            <div class="feature-grid">
+                ${innerHTML}
+            </div>
+            `;
+
+        // Set Figure Placeholders.
+		setFigures("https://cyberespaco.github.io/assets/images/vaporwave/img");
+    });
+}
