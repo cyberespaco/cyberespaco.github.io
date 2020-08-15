e0e4e1b46d49c739e6d9e4bef032b8d580308cbc
Pedro <pedromxavier@poli.ufrj.br>
2020-08-15 00:20:09 -0300

    Ok

--- assets/js/archive.js
+++ assets/js/archive.js
@@ -22,10 +22,47 @@ function buildArchive(filePath) {
     $.getJSON( filePath, function ( array ) {
 
         let element = document.getElementById("archive");
-        var innerHTML = new String();
+        let innerHTML = "";
+
+        let icons = ["github", "youtube", "link", "mail"];
 
         for (let i=0; i<array.length; i++) {
             let json = array[i];
+
+            let iconsHTML = "";
+
+            for (let j=0; j<icons.length; j++) {
+                let icon = icons[j];
+
+                if (!(icon in json)){
+                    continue
+                }
+
+                let HTML = "";
+
+                switch(icon) {
+                    case "github":
+                        HTML = `<li><a href="${archiveGet(json, "github")}" class="icon fa-github"><span class="label">Github</span></a></li>
+                        `;
+                        break;
+                    case "youtube":
+                        HTML = `<li><a href="${archiveGet(json, "youtube")}" target="_blank" class="icon fa-youtube-play"><span class="label">YouTube</span></a></li>
+                        `;
+                        break;
+                    case "link":
+                        HTML = `<li><a href="${archiveGet(json, "link")}" class="icon fa-link"><span class="label">Link</span></a></li>
+                        `;
+                        break;
+                    case "mail":
+                        HTML = `<li><a href="${archiveGet(json, "mail")}" class="icon fa-envelope-o"><span class="label">Mail</span></a></li>
+                        `;
+                        break;
+                    default:
+                        break;
+                }
+
+                iconsHTML = iconsHTML + HTML
+            }
             
             innerHTML = innerHTML + `
             <div class="feature">
@@ -39,10 +76,7 @@ function buildArchive(filePath) {
                     </header>
                     <p>${archiveGet(json, "text")}</p>
                     <ul class="icons">
-                        <li><a href="${archiveGet(json, "github")}" class="icon fa-github"><span class="label">Github</span></a></li>
-                        <li><a href="${archiveGet(json, "youtube")}" target="_blank" class="icon fa-youtube-play"><span class="label">YouTube</span></a></li>
-                        <li><a href="${archiveGet(json, "link")}" class="icon fa-link"><span class="label">Link</span></a></li>
-                        <li><a href="${archiveGet(json, "mail")}" class="icon fa-envelope-o"><span class="label">Mail</span></a></li>
+                        ${iconsHTML}
                     </ul>
                 </div>
             </div>
